4114f3cb2b181892702d1eac9cdf1bfd91ee3fd2
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-11 13:48:34 +0100

    Ho creato l'array con 10 numeri

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,3 +2,9 @@ console.log('js ok');
 
 /* Crea un array di numeri interi
 fai la somma di tutti gli elementi che sono in posizione dispari. */
+
+const numbersArray = [2, 3, 45, 67, 46, 77, 86, 49, 43, 14, 3];
+
+console.log("all'interno dell'array ci sono:", numbersArray.length, "numeri");
+
+
