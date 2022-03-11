da4e4da98c34584422d77a87c16a748c42f69013
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-11 14:54:06 +0100

    Ho stampato la somma dei numeri posizione dispari

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -7,4 +7,16 @@ const numbersArray = [2, 3, 45, 67, 46, 77, 86, 49, 43, 14, 3];
 
 console.log("all'interno dell'array ci sono:", numbersArray.length, "numeri");
 
+let somma = 0;
+
+for (let index = 0; index < numbersArray.length; index++) {
+    if (index % 2 !== 0) {
+        somma += numbersArray[index];
+
+        console.log(numbersArray[index]);
+    }
+}
+console.log("La somma dei numeri in posizione dispari è:", somma);
+
+
 
