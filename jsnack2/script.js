635fda7592074acbace4bccb49f9a407feb934a9
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-11 15:19:48 +0100

    Ho creato il prompt per i 5 numeri

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -3,3 +3,21 @@ console.log('js ok');
 /* Il software deve chiedere per 5 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. 
 Esegui questo programma in due versioni, con il for e con il while. */
+
+// creo un array vuoto in cui inserire i numeri 
+const emptyArray = [];
+
+// dichiaro la somma dei numeri inseriti dall'utente
+let somma = 0; 
+
+for (let i = 0; i < 5; i++){
+
+    // chiedo i 5 numeri all'utente
+    let numberSelected = parseInt(prompt(`inserisci 1 numero`));
+
+    // dai numeri inseriti faccio la somma
+    somma += numberSelected;
+
+}
+
+console.log(somma);
